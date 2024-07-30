537119c1d0e4b44eb8632c7ca39c77dc4290ca7b
HetVaishnani <hetvaishnani0709@gamil.com>
2024-07-30 09:43:59 +0530

    my code

---
+++ day-18/14.js
@@ -0,0 +1,9 @@
+let car = {
+    "brand": "Toyota",
+    "model": "Corolla",
+    "year": 2022,
+};
+
+for (let k in car) {
+    console.log(`${k} : ${car[k]}`);
+}
---
+++ day-18/15.js
@@ -0,0 +1,9 @@
+let book = {
+    "title": "Harry Potter",
+    "author": "J.K. Rowling",
+    "pages": 400,
+};
+
+for (let k in book) {
+    console.log(`${k} : ${book[k]}`);
+}
